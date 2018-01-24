9b0f65ad55d513e6418b25f9cd5052e0e3c73301
Joseph Kieffer <joseph.kieffer@hotmail.fr>
2018-01-24 12:12:51 +0100

    Addd support for text serialization of localized strings

--- Assets/Scripts/Engine/Engine/Editor/CustomEditors/LocalizedStringBuilder.cs
+++ Assets/Scripts/Engine/Engine/Editor/CustomEditors/LocalizedStringBuilder.cs
@@ -47,6 +47,10 @@ namespace Engine
 	[System.Serializable]
 	public abstract class LocalizedStringBuilder<T> : EditorWindow
 	{
+		// Change me to save as binary or as text
+		private const bool kSaveBinary = false;
+		private const bool kLoadBinary = false;
+
 		private const string kTempPath = "Assets/Temp/LocalizedStrings/";
 		private const int kStringsPerWindow = 20;
 
@@ -98,34 +102,14 @@ namespace Engine
 			for ( int lang = 0; lang < ( int )LocalizedString.Lang.COUNT; ++lang )
 			{
 				string path = kTempPath + System.Enum.GetName( typeof( LocalizedString.Lang ), ( ( LocalizedString.Lang )lang ) ) + ".str";
-				FileStream fileStream = null;
-				try
+				if ( kLoadBinary )
 				{
-					fileStream = new FileStream( path, FileMode.Open );
+					BinLoad( lang, path );
 				}
-				catch ( System.Exception e )
+				else
 				{
-					Debug.Log( "Unable to open file " + path + ":\n" + e.Message );
-					return;
+					TxtLoad( lang, path );
 				}
-				BinaryReader reader = new BinaryReader( fileStream );
-				long length = reader.ReadInt64();
-				for ( int i = 0; i < length; ++i )
-				{
-					string key = reader.ReadString();
-					string value = reader.ReadString();
-					try
-					{
-						int index = ( int )System.Enum.Parse( typeof( T ), key );
-						m_array[ index ][ lang ] = value;
-					}
-					catch
-					{
-						Debug.Log( "Warning! The string " + key + " is no more a part of the enum..." );
-						Debug.Log( "If you want to recover it, read this value to the " + typeof(T).Name + " enum" );
-					}
-				}
-				reader.Close();
 			}
 		}
 		private void Save()
@@ -133,24 +117,148 @@ namespace Engine
 			for ( int lang = 0; lang < ( int )LocalizedString.Lang.COUNT; ++lang )
 			{
 				string path = kTempPath + System.Enum.GetName( typeof( LocalizedString.Lang ), ( ( LocalizedString.Lang )lang ) ) + ".str";
-				FileStream fileStream = new FileStream( path, FileMode.OpenOrCreate );
-				BinaryWriter writer = new BinaryWriter( fileStream );
-				writer.Write( ( long )m_array.Length );
-				for ( int str = 0; str < m_array.Length; ++str )
+				if ( kSaveBinary )
 				{
-					writer.Write( System.Enum.GetName( typeof( T ), str ) );
-					if ( m_array[ str ][ lang ] == null )
-					{
-						writer.Write( "" );
-					}
-					else
-					{
-						writer.Write( m_array[ str ][ lang ] );
-					}
+					BinSave( lang, path );
 				}
-				writer.Close();
+				else
+				{
+					TxtSave( lang, path );
+				}
 			}
 			AssetDatabase.Refresh( ImportAssetOptions.ForceUpdate );
+		}
+		private void BinSave( int _lang, string _path)
+		{
+			FileStream fileStream = new FileStream( _path, FileMode.OpenOrCreate );
+			BinaryWriter writer = new BinaryWriter( fileStream );
+			writer.Write( 'B' );
+			writer.Write( 'I' );
+			writer.Write( 'N' );
+			writer.Write( ( long )m_array.Length );
+			for ( int str = 0; str < m_array.Length; ++str )
+			{
+				writer.Write( System.Enum.GetName( typeof( T ), str ) );
+				if ( m_array[ str ][ _lang ] == null )
+				{
+					writer.Write( "" );
+				}
+				else
+				{
+					writer.Write( m_array[ str ][ _lang ] );
+				}
+			}
+			writer.Close();
+		}
+		private void BinLoad( int _lang, string _path )
+		{
+			FileStream fileStream = null;
+			try
+			{
+				fileStream = new FileStream( _path, FileMode.Open );
+			}
+			catch ( System.Exception e )
+			{
+				Debug.Log( "Unable to open file " + _path + ":\n" + e.Message );
+				return;
+			}
+			BinaryReader reader = new BinaryReader( fileStream );
+			byte[] type = reader.ReadBytes( 3 );
+			if ( type[ 0 ] != 'B' || type[ 1 ] != 'I' || type[ 2 ] != 'N')
+			{
+				Debug.Log( "The localized string file is not in binary format!" );
+				reader.Close();
+				return;
+			}
+			long length = reader.ReadInt64();
+			for ( int i = 0; i < length; ++i )
+			{
+				string key = reader.ReadString();
+				string value = reader.ReadString();
+				try
+				{
+					int index = ( int )System.Enum.Parse( typeof( T ), key );
+					m_array[ index ][ _lang ] = value;
+				}
+				catch
+				{
+					Debug.Log( "Warning! The string " + key + " is no more a part of the enum..." );
+					Debug.Log( "If you want to recover it, read this value to the " + typeof( T ).Name + " enum" );
+				}
+			}
+			reader.Close();
+		}
+		private void TxtSave( int _lang, string _path )
+		{
+			FileStream fileStream = new FileStream( _path, FileMode.OpenOrCreate );
+			StreamWriter writer = new StreamWriter( fileStream );
+			writer.WriteLine( "TXT" );
+			for ( int str = 0; str < m_array.Length; ++str )
+			{
+				writer.Write( System.Enum.GetName( typeof( T ), str ) + ":" );
+				if ( m_array[ str ][ _lang ] == null )
+				{
+					writer.Write( "\"\"" );
+				}
+				else
+				{
+					string correctString = "\"" + m_array[ str ][ _lang ].Replace( "\"", "\\\"" ) + "\"";
+					writer.WriteLine( correctString );
+				}
+			}
+			writer.Close();
+		}
+		private void TxtLoad( int _lang, string _path )
+		{
+			FileStream fileStream = null;
+			try
+			{
+				fileStream = new FileStream( _path, FileMode.Open );
+			}
+			catch ( System.Exception e )
+			{
+				Debug.Log( "Unable to open file " + _path + ":\n" + e.Message );
+				return;
+			}
+			StreamReader reader = new StreamReader( fileStream );
+			string type = reader.ReadLine();
+			if ( type != "TXT" )
+			{
+				Debug.Log( "The localized string file is not in text format!" );
+				reader.Close();
+				return;
+			}
+			int lineNumber = 0;
+			while ( !reader.EndOfStream )
+			{
+				++lineNumber;
+				string line = reader.ReadLine().Trim();
+				if ( line.Length == 0 )
+				{
+					continue;
+				}
+				int indexSeparator = line.IndexOf( ':' );
+				if ( indexSeparator < 0 )
+				{
+					Debug.Log( "Error: Syntax error, missing ':' separator line " + lineNumber );
+					reader.Close();
+					return;
+				}
+				string key = line.Substring( 0, indexSeparator );
+				string value = line.Substring( indexSeparator + 1 );
+				value = value.Substring( 1, value.Length - 2 );
+				try
+				{
+					int index = ( int )System.Enum.Parse( typeof( T ), key );
+					m_array[ index ][ _lang ] = value;
+				}
+				catch
+				{
+					Debug.Log( "Warning! The string " + key + " is no more a part of the enum..." );
+					Debug.Log( "If you want to recover it, read this value to the " + typeof( T ).Name + " enum" );
+				}
+			}
+			reader.Close();
 		}
 		private void Export()
 		{
