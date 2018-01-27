9a840ab10c16fcbbefc209f9dafd5953008082a6
Amelia <amelia@mysteriousfox.com>
2018-01-27 10:28:56 +0100

    Added wind movement to the mill. =)

---
+++ Assets/Scripts/Game/Scene/WindMillScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMillScript : MonoBehaviour {
+
+	public GameObject wind;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		wind.transform.Rotate(speed * Time.deltaTime, 0.0f, 0.0f);
+	}
+}
