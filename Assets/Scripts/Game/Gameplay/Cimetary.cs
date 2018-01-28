cd08ef59ddfcdab9b01e1aa429adc611905b158e
Joseph Kieffer <joseph.kieffer@hotmail.fr>
2018-01-28 15:33:16 +0100

    Cimetery music + dog sound

---
+++ Assets/Scripts/Game/Gameplay/Cimetary.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * LICENCE
+ */
+using UnityEngine;
+
+[System.Serializable]
+public class Cimetary : Engine.BehaviourBase
+{
+	public override void Trigger( Engine.TriggerBase _trigger, Object _data )
+	{
+		if ( GameMusicManager.Instance.m_ActualMusic != GameMusicManager.EGameMusicManagerState.eFaucheuse )
+		{
+			GameMusicManager.Instance.ChangeMusic( GameMusicManager.EGameMusicManagerState.eFaucheuse );
+		}
+	}
+}
